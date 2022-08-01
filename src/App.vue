5edfec4e3c1ea322fa4b9e8941cc86f361bee31a
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-08-01 08:57:14 -0300

    finalizando projeto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ContactForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContactForm from '@/components/ContactForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContactForm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body, html, #app {
+        width: 100%;
+        height: 100%;
+    }
 </style>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="w-100 h-100 d-flex flex-column align-items-center justify-content-around">
+        <div class="text-center">
+            <h2 class="text-success">Send Mail Template</h2>
+            <p class="text-secondary">Send mail from a Landing Page With VueJs and EmailJS</p>
+        </div>
+        <form @submit.prevent="sendMail($event)" class="row container">
+            <div class="col-sm-6 py-2">
+                <label class="form-label">Name</label>
+                <input 
+                    class="form-control"
+                    type="text" 
+                    v-model="name"
+                    name="name"
+                    placeholder="Your Name"
+                >
+            </div>
+            
+            <div class="col-sm-6 py-2">
+                <label class="form-label">Email</label>
+                <input 
+                    class="form-control"
+                    type="text" 
+                    v-model="email"
+                    name="email"
+                    placeholder="Your Email"
+                >
+            </div>
+
+            <div class=" py-2">
+                <label class="form-label">Message</label>
+                <textarea 
+                    class="form-control"
+                    name="message" 
+                    v-model="message"
+                    cols="30" rows="5"
+                    placeholder="Message">
+                </textarea>
+            </div>
+
+            <input type="submit" value="Send" class="btn btn-success col-3 mx-auto mt-4">
+        </form>
+
+        <div>
+            Created With ❤ by <a href="https://isaaclouzeiro.dev.br" target="_blank" class="link-item">Isaac Louzeiro</a>
+        </div>
+    </div>
+</template>
+
+<script>
+import emailjs from 'emailjs-com';
+
+export default {
+    name: 'ContactForm',
+    data() {
+        return {
+            name: '',
+            email: '',
+            message: ''
+        }
+    },
+    methods: {
+        sendMail(e) {
+            try {
+                emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_SERVICE_ID', e.target, 'YOUR_SERVICE_ID', {
+                    name: this.name,
+                    email: this.email,
+                    message: this.message
+                })
+            } catch(error) {
+                console.log({error})
+            }
+
+            // reset
+            this.name = ''
+            this.email = ''
+            this.message = ''
+        }
+    }
+}
+</script>
